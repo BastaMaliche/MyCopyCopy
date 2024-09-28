bcb1a2998f3257d1e8f73f5b8849e900723c0bb1
WS22 <malichevincentloyd7@gmail.com>
2024-09-28 11:17:01 +0800

    mana copy

---
+++ task1fortoday.py
@@ -0,0 +1,15 @@
+age = [-12,13,-14,15-16]
+username = ["juan","james","sagaad","fuego"]
+for dataage in age:
+    if dataage <0:
+        print(str(dataage) + "negative")
+    elif dataage >0:
+        print(str(dataage) + "positive")
+
+
+        for datausername in username:
+            if datausername == "james":
+                print("james cute")
+    else:
+        print("james none")
+        break
